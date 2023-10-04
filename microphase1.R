99e8796549e9633ef22d3de5617c22c0b4b9fff1
sisalt19 <125088763+sisalt19@users.noreply.github.com>
2023-10-03 17:30:45 -0700

    Add files via upload

--- microphase1.R
+++ microphase1.R
@@ -1,6 +1,7 @@
 # Analysis of IRES Litter Microclimate Group data for microphase 1 experiment
 # HThroop wrote script on August 2, 2022 
 # HThroop made minor tweaks on Sept 20, 2022
+# SSmamia rewrote script Sept 25, 2023
 setwd("/Users/Siena/Desktop/microclimate")
 
 library(tidyverse)
@@ -10,18 +11,7 @@ my_colors <- c("#fa882a","#6e4424","#5dba2b","#166610")
 
 #import dataset
 df_microphase1=read.csv("microphase1.csv",header=TRUE)
-# remove interval 13 - these were extras (alternative would be to treat them as interval 12 - they were collected then)
-#df_microphase1<-df_microphase1[!(df_microphase1$Interval > 12),]
-# treat rainfall as a factor (categorical variable)
 df_microphase1$Rainfall <- factor(df_microphase1$Rainfall) 
-
-#df_microphase1$Datetime <- as.POSIXct(df_microphase1$Datetime, format = "%m/%d/%y %H:%M")
-
-# change time from 12 h to 24 h time
-#df_microphase1$Time <- format(as.POSIXct(df_microphase1$Time,format='%I:%M:%S %p'),format="%H:%M:%S")
-
-# create a new variable "datetime" that combines data and time
-#df_microphase1$datetime <- as.POSIXct(paste(df_microphase1$Time, df_microphase1$Date), format="%m-%d-%y %H:%M:%S")
 
 
 # calculate litter wet and dry masses
@@ -33,12 +23,6 @@ df_microphase1$LitterMoisture <- ((df_microphase1$LitterWetMass - df_microphase1
 df_microphase1.SAPE <- filter(df_microphase1, Species == "SAPE")
 df_microphase1.STSA <- filter(df_microphase1, Species == "STSA")
 
-# make some line graphs ---------------------------------------------------
-# ylim.prim <- c(0, 150)   # in this example, precipitation
-# ylim.sec <- c(0, 50)    # in this example, temperature
-
-
-
 
 # calculate mean values for line graphs 
 df_microphase1.means <- df_microphase1 %>% 
@@ -48,19 +32,12 @@ df_microphase1.means$se<-as.numeric(df_microphase1.means$se)
 df_microphase1.means$LitterMoisture_mean<-as.numeric(df_microphase1.means$LitterMoisture_mean)
 df_microphase1.means
 
-# exclude values above and below the 95th and 5th percentile
-#microphase1.means_quantiles <- quantile(df_microphase1.means$LitterMoisture_mean, c(0.05, 0.95))
-#microphase1.means_quantiles
-#data_subset <- df_microphase1.means[df_microphase1.means$LitterMoisture_mean > microphase1.means_quantiles[1] &   # Drop rows below/above percentiles
-                         #df_microphase1.means$LitterMoisture_mean < microphase1.means_quantiles[2], ]
-
-#df_microphase1.means$Cond_spec_num <- as.numeric(substr(df_microphase1.means$Cond_spec,2,))
 
 p1 <- ggplot(df_microphase1.means, aes(x=Datetime, y=LitterMoisture_mean, color=Cond_spec)) + 
   aes(group=Cond_spec) +
   geom_errorbar(aes(ymin=LitterMoisture_mean-se, ymax=LitterMoisture_mean+se), width=0.1) +
-  geom_line(aes(color=Cond_spec, shape=Cond_spec, linetype=Cond_spec)) +
-  geom_point() +
+  geom_line(aes(color=Cond_spec, linetype=Cond_spec)) +
+  geom_point(aes(shape=Cond_spec)) +
   annotate('rect', xmin=7, xmax=8.5, ymin=-5, ymax=150, alpha=.5, fill='#54b8e3') +
   xlab("Collection Time") + 
   ylab("Litter Moisture (% by mass)") +
@@ -70,10 +47,10 @@ p1 <- ggplot(df_microphase1.means, aes(x=Datetime, y=LitterMoisture_mean, color=
   theme(panel.grid.major = element_blank()) +
   theme(panel.grid.minor = element_blank()) +
   scale_y_continuous(expand=c(0,0)) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   facet_grid(~Rainfall) 
 p1
 
-p1 + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 
 
