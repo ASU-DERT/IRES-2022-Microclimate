bfe0d945119bb2905fb2dbf9f0d90fb8f24b2014
Heather Throop <heather.throop@asu.edu>
2023-10-10 18:44:01 -0700

    Located POSIXct issue (see comments) and fixed line width in p1.

--- ibuttons_phase1_plots.R
+++ ibuttons_phase1_plots.R
@@ -18,9 +18,6 @@ my_colors <- c("#fa882a","#6e4424","#5dba2b","#166610")
 
 #read csv with manually made datetime column
 df_Temps1=read.csv(here('Microphase1_iButtons_ALL_dt.csv'))
-# HT: the data type for datetime is 'chr' -- we need to explictly define this as a datetime format
-# use 'as_datetime' from lubridate to change Datetime to POSIXct format
-df_Temps1$datetime <- as_datetime(df_Temps1$datetime, format = "%m/%d/%y %H:%M", tz = "Africa/Johannesburg")
 
 
 #add rainfall based on quadrat
@@ -46,6 +43,9 @@ df_Temps1 <- df_Temps1 %>%
 df_Temps1$datetime <- as.POSIXct(df_Temps1$datetime, format = "%m/%d/%y %H:%M")
 df_Temps1 <- df_Temps1 %>% 
   mutate(interval = cut(datetime, breaks = "60 min"))
+# I think this mutate step is causing the issue. Interval is a factor rather than a POSIXct and we can't convert to POSIXct since 
+# some of the values for interval have a time and some do not. I'm not sure why it drops the time for the 00:00:xx values -- see
+# if you can fix than and the rest of the problem should go away!
 
 # calculate mean values for line graphs 
 df_Temps1.means <- df_Temps1 %>% 
@@ -58,7 +58,7 @@ df_Temps1.means
 p1 <- ggplot(df_Temps1.means, aes(x=interval, y=Temp_mean, color=Type)) + 
   aes(group=Type) +
   geom_errorbar(aes(ymin=Temp_mean-se, ymax=Temp_mean+se), width=0.1) +
-  geom_line(aes(color=Type, linetype=Type)) +
+  geom_line(aes(color=Type, linetype=Type), size = 0.9) + # move size outside of aes statement to control line width
   geom_point(size=0.5) +
   xlab("Time") + 
   ylab("Temperature (ºC)") +
@@ -68,7 +68,8 @@ p1 <- ggplot(df_Temps1.means, aes(x=interval, y=Temp_mean, color=Type)) +
   theme(panel.grid.major = element_blank()) +
   theme(panel.grid.minor = element_blank()) +
   scale_y_continuous(expand=c(0,0)) +
-  #scale_x_datetime(date_labels = "%m/%d/%y %H:%M",breaks = date_breaks("2 hours"))+
+ # scale_x_datetime(breaks = "2 hours") +
+ # scale_x_datetime(date_labels = "%m/%d/%y %H:%M",breaks = date_breaks("2 hours")) +
   #theme(axis.text.x = element_text(angle = 90, vjust = 1.0, hjust = 1.0)) +
   #scale_x_datetime(labels=format("%H:%M"), expand=c(0,0)) +
   #xlim(c(as.POSIXct('07/24/22 11:00', format = "%m/%d/%y %H:%M"),
